fd52bc80d826d7190f6fde4e56e47479910fe898
ayak973 <ayak973@protonmail.com>
2017-12-05 21:20:59 +0100

    Update Makefile

--- TinyTCPClient/main.cpp
+++ TinyTCPClient/main.cpp
@@ -1,18 +1,60 @@
 /* 
  * File:   main.cpp
- * Author: ayak973
+ * Author: Ayak973
  *
  * Created on December 5, 2017, 19:01
  */
 
 #include <iostream>
+#include <string>
+#include <array>
+#include <thread>
+
+#include<sys/socket.h>	//socket
+#include<arpa/inet.h>	//inet_addr
+#include<netdb.h>	//hostent
 
 using std::cout;
 using std::endl;
 
+static const unsigned int parallelJobsCount = 4;
+static const int port = 8080;
+static const std::string host = "127.0.0.1";
+static const std::string sendStr = "Hello";
+
+
+void sendAndReceive(int threadId);
 
 int main(int argc, char** argv) {
-    cout << "here" << endl;
+    cout << "Starting client..." << endl;
+    
+    //socket file descriptor array
+    std::array<int, parallelJobsCount> fdArray = { 0 };
+    std::array<std::thread, parallelJobsCount> threadArray;
+    
+    for (unsigned int i = 0; i < parallelJobsCount; ++i) {
+        
+        threadArray[i] = std::thread(sendAndReceive, i);
+        
+    }
+
+    for (unsigned int i = 0; i < parallelJobsCount; ++i) {
+        
+        if (threadArray[i].joinable()) threadArray[i].join();
+        
+    }
+    
+    cout << "Client finished !" << endl;
+    
     return 0;
 }
+
+void sendAndReceive(int threadId) {
+    cout << "Starting thread #" << threadId << "..." << endl;
+    
+    
+    
+    cout << "Thread #" << threadId << " stopped !" << endl;
+}
+
 
