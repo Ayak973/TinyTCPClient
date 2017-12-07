cc2fc3c7b1d6939fce5be73451d5cf1a5c1cb568
Ayak973 <ayak973@protonmail.com>
2017-12-07 17:21:35 +0100

    Socket base class modifications Added socket.cpp for implementation

--- TinyTCPClient/socket.cpp
+++ TinyTCPClient/socket.cpp
@@ -1,12 +1,16 @@
 #include "socket.h"
 
+#include <sys/types.h>
 #include <sys/socket.h>
+
 
 using namespace tinytcp;
 
 Socket::Socket(int type, int domain, int protocol) : _fd(-1) {
-    ::socket(type, domain, protocol);
-    
+    _fd = ::socket(type, domain, protocol);
+    if (_fd == -1) {
+        
+    }
 }
 
 int Socket::getSocket() const {
